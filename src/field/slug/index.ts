8743b4c137db692c2b4335afa61e8c4b8460cf73
isphinx <isphinx@me.com>
2024-08-31 18:15:17 +1000

    fix

--- src/field/slug/index.ts
+++ src/field/slug/index.ts
@@ -40,7 +40,7 @@ export const slugField: Slug = (fieldToUse = 'title', overrides = {}) => {
       ...(slugOverrides?.admin || {}),
       components: {
         Field: {
-          path: 'node_modules/mmg-payload-plugins/dist/#SlugComponent',
+          path: 'node_modules/mmg-payload-plugins/client/#SlugComponent',
           clientProps: {
             fieldToUse,
             checkboxFieldPath: checkBoxField.name,
